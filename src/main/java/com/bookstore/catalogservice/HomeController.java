ba4723392b86c2c7182e2430ab17bd07dcccb0c1
liarreez <liarreez@gmail.com>
2024-06-23 03:59:08 +0900

    initial commit

--- src/main/java/com/bookstore/catalogservice/HomeController.java
+++ src/main/java/com/bookstore/catalogservice/HomeController.java
@@ -1,13 +1,20 @@
 package com.bookstore.catalogservice;
 
+import com.bookstore.catalogservice.config.StoreProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final StoreProperties storeProperties;
+
+    public HomeController(StoreProperties storeProperties) {
+        this.storeProperties = storeProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "도서 카탈로그에 오신 것을 환영합니다!";
+        return storeProperties.getGreeting();
     }
 }
